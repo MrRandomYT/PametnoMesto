320be2c74c90f031d5ba71358376641837a5cca6
MrRandomYT <73438313+MrRandomYT@users.noreply.github.com>
2025-11-13 12:37:21 +0100

    Build YML FIx?
    Fixed Hopefully

--- PametnoMesto.Test/obj/Debug/net8.0/PametnoMesto.Test.AssemblyInfo.cs
+++ PametnoMesto.Test/obj/Debug/net8.0/PametnoMesto.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PametnoMesto.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25e8c766011c4cdb3151b8b1a3f193ae79ddb6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b596dbc8d70c94bbb33c0c0bc96d378fbdea5f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("PametnoMesto.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PametnoMesto.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
