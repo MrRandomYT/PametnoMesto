1dfd8fb00ffa44719edc1e848775480bddb3eedd
MrRandomYT <73438313+MrRandomYT@users.noreply.github.com>
2025-12-11 12:11:21 +0100

    100% Body

--- PametnoMesto/obj/Debug/net8.0/PametnoMesto.AssemblyInfo.cs
+++ PametnoMesto/obj/Debug/net8.0/PametnoMesto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PametnoMesto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9e68fc6b8445220266836835a2098d3a1cbbe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0278375c85e1f297b7aea44e55061dd7b1806590")]
 [assembly: System.Reflection.AssemblyProductAttribute("PametnoMesto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PametnoMesto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
